c29acbc4fde820420cfed990ca8a4d7f3b43fa65
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-10-19 21:59:07 -0500

    gifs service

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  constructor(
+    private gifsService: GifsService
+  ) { }
+
+  search(){
+    const value = this.txtSearch.nativeElement.value;
+    if(!value.trim()) return;
+    this.gifsService.searchGifs(value);
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
