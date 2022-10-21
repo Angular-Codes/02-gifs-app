739e2f9f05f5981d7eba99e87534c0a0aee7d015
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-10-20 23:08:47 -0500

    call service

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
 
 @Component({
   selector: 'app-results',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent {
+
+  get results() {
+    return this.gifsService.results;
+  }
+
+  constructor(
+    private gifsService: GifsService
+  ) { }
+
 
 }
--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private API_KEY: string = 'OXiP38i6EBocYAuuHcIdORbjXhE5V92J';
   private _history: string[] = [];
+
+  public results: Gif[] = [];
 
   get history() {
     return [...this._history];
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   searchGifs(query: string = '') {
     query = query.trim().toLowerCase();
@@ -19,7 +26,17 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
       this._history = this._history.splice(0, 10);
+      this.loadGifs(query);
     }
   }
+
+  private loadGifs(query: string = '') {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.API_KEY}&q=${query}&limit=10`)
+      .subscribe((resp) => {
+        this.results = resp.data;
+      });
+  }
+
+
 
 }
